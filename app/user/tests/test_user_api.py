7e6583aeba3046fe3056f6dda00268a2f2f9d5b2
christojohnsonv <christojohnsonv@gmail.com>
2023-03-14 23:33:17 +0530

    Updated: Flake issues fixed.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -14,6 +14,7 @@ TOKEN = reverse('user:token')
 
 # def create_user(**params)
 
+
 class PublicUserApiTests(TestCase):
     """Tests the public features of the user api."""
 
@@ -34,7 +35,7 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
-    
+
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
@@ -46,7 +47,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_password_too_short_error(self):
         """Test an error if password less than five charectors."""
         payload = {
